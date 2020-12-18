64623ef2885a7cc9def074a7573829abb3bb4c7e
Luis Saldana <tluis10a@gmail.com>
2020-12-17 20:21:39 -0800

    adding the structured english

---
+++ IS51Final.py
@@ -0,0 +1,10 @@
+""" 
+starting off this program will be opening a text file and try to compute the number of grades there are and then using a function we will try to get an average on the previous answer.
+We will write a program that will display the number of grades 
+having file.txt in root file open with the formula open() method to retrieve it from the final.txt file 
+another thing is it will also tell us about the average of the grades using the kickstart function
+then it will show the percentage of grades that are above the average grade from the total grades
+after we will use calculate percent above average function to make calculations
+we will then assign grades to a list of intergers to be able to get the number of grades with an average grade and also the percentage
+
+"""
